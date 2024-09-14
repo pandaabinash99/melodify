c8cefd054ad576e47a927df245b05e4ee2e1b830
Abinash Panda <apanda@greenstreet.com>
2024-09-14 16:25:09 +0530

    initial setup and cleanup

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+export default {
+  semi: false,
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  jsxSingleQuote: false,
+  bracketSameLine: false,
+  endOfLine: 'lf',
+}
